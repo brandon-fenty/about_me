ce9e8829493b60b836384e23b2a68a321aa0dbd5
brandon-fenty <brandonfenty@gmail.com>
2018-05-01 14:00:14 -0700

    Question 1 complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 'use strict';
 
-alert('ITS ALIVE!!!!');
+// Ask for the users name
+var userName = prompt('Hi! What is your name?');
+
+// Greet the user
+alert('Hello, ' + userName + ' , welcome!');
+
+// Log that interaction to console
+console.log('The user answered ' + userName);
+
+// Ask the first guessing game question
+var answer1 = prompt('Do you think I have any siblings?').toLowerCase();
+// User guesses "yes/no"
+if (answer1 === 'yes' || answer1 === 'y') {
+    alert('That is correct');
+    console.log('The user got the correct answer to #1, which is yes');
+} else {
+    alert('Sorry, that is incorrect');
+    console.log('The user got the answer wrong to #1');
+}
+
+// Tell user if they are wrong or right
+
+// Next question
