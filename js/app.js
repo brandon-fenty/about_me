3d630664c993dbe09f8cbdf4af0a3cc2f61b11fa
brandon-fenty <brandonfenty@gmail.com>
2018-05-03 06:33:44 -0700

    Added new questions, array not yet working

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@
 
 // Ask for the users name
 var userName = prompt('Hi! What is your name?');
-
 // Greet the user
 alert('Hello ' + userName + ', welcome to my guessing game! Lets see if you really know me.');
-
 // Log that interaction to console
 console.log('The user answered ' + userName);
 
@@ -53,8 +51,68 @@ if (answer4 === 'yes' || answer4 === 'y') {
 var answer5 = prompt('Do you think I like country music?').toLowerCase();
 if (answer5 === 'no' || answer5 === 'n') {
     alert('Thats correct, I dabble in audio engineering and love most music but country music is one of the only genres I cant stand!');
-    console.log('The user got the answer correct, which is no');
+    console.log('The user got the answer to #5 correct, which is no');
 } else {
     alert('Sorry, but thats incorrect, as an audio engineer, I refused to record country music because I cannot stand it.');
-    console.log('The user got the answer incorrect')
+    console.log('The user got the answer incorrect');
+}
+
+// 6th question (number guess)
+var counterQ6 = 4;
+var userWrong = true;
+// Begin loop
+while(counterQ6 > 0 && userWrong === true) {
+    var answer6 = prompt('How many states do you think I have lived in? You have 4 chances to get it right').toLowerCase();
+    // For correct answer
+    if(answer6 === '3') {
+        alert('Wow, nice guess! I have lived in Washington, Cali, and Hawaii');
+        console.log('The user got the answer to #6 correct');
+        userWrong = false;
+    }
+    // If answer is too low
+    else if(answer6 < '3') {
+        alert('Sorry, thats a little low! You have ' + counterQ6 + ' tries left.');
+        console.log('The user answered ' + answer6 + ' for Q6 which is incorrect');
+    } 
+    // If answer is too high
+    else if(answer6 > '3') {
+        alert('Sorry, thats a little high! You have ' + counterQ6 + ' tries left.');
+        console.log('The user answered ' + answer6 + ' for Q6 which is incorrect');
+    }
+    counterQ6--; 
+    // Statement to end loop
+    if(counterQ6 === 0 && answer6 !== 3) {
+        alert('Sorry, you ran out of guesses! The correct answer was 3, WA, CA, and HI');
+        console.log('The user ran out of guesses without getting the correct answer');
+    }
+}
+
+// 7th question (array)
+var arrayAnswers = ['thai', 'pizza', 'pho', 'burritos', 'steak'];
+var userWrong = true;
+var numGuesses = 5;
+
+// Begin loop
+while(numGuesses > -1 && userWrong === true) {
+        
+     // Begin checking array
+     for(var index = 0; index < arrayAnswers.length; index++) {
+        var answer7 = prompt('Can you guess one of my favorite foods? You will have 6 tries to get one of them').toLocaleLowerCase();   
+         if(answer7 === arrayAnswers[index]){
+            alert('Nicely done! But then again, everyone loves ' + answer7 + ' right?');
+            console.log('The user answered #7 correctly with ' + numGuesses + ' left');
+            userWrong = false;
+        } else if(answer7 !== arrayAnswers[index]){
+            alert('Sorry, thats not one of my faves! Keep trying though since you have ' + numGuesses + ' guesses left');
+            console.log('The user answered #7 incorrectly they have ' + numGuesses + ' guesses left');
+        }
+        // End loop
+        numGuesses--;
+
+        if(numGuesses === 0 && userWrong === true){
+                alert('Sorry you ran out of guesses! Better luck next time');
+                console.log('The user ran out of guesses for #7')
+        }
+    }
+
 }
